8766a3c4aa456301bf36ce5a90b0c3fddba7af30
Raditya Bani Ainur Ridho <149133820+rradz11@users.noreply.github.com>
2025-05-23 08:30:55 +0700

    Update birthdays.dart

--- lib/birthdays.dart
+++ lib/birthdays.dart
@@ -6,12 +6,155 @@ import 'main.dart';
 import 'your_when.dart';
 
 class BirthdaysPage extends StatefulWidget {
-  final List<Map<String, dynamic>> whenList;
-
-  const BirthdaysPage({super.key, required this.whenList});
+  const BirthdaysPage({super.key});
 
   @override
   State<BirthdaysPage> createState() => _BirthdaysPageState();
+}
+
+class AddBirthdayPage extends StatefulWidget {
+  const AddBirthdayPage({super.key});
+
+  @override
+  State<AddBirthdayPage> createState() => _AddBirthdayPageState();
+}
+
+class _AddBirthdayPageState extends State<AddBirthdayPage> {
+  final TextEditingController _titleController = TextEditingController();
+  DateTime? selectedDate;
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  void _addWhen() {
+    final String title = _titleController.text.trim();
+    if (title.isEmpty || selectedDate == null) return;
+
+    final newBirthday = {
+      'title': title,
+      'category': 'Birthdays',
+      'date': selectedDate,
+    };
+    globalWhenList.add(newBirthday);
+    Navigator.pop(context, newBirthday); // kirim balik ke halaman sebelumnya
+    saveWhenToHive(); // simpan ke Hive
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF213339),
+      appBar: AppBar(
+        title: Text(
+          "Add New Birthday",
+          style: GoogleFonts.dmSans(
+            color: Color(0xFFEAFFF9),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: const Color(0xFF2F4B53),
+        foregroundColor: const Color(0xFFEAFFF9),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Write someone's Birthday:",
+              style: GoogleFonts.dmSans(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFFEAFFF9),
+              ),
+            ),
+            const SizedBox(height: 12),
+            TextField(
+              controller: _titleController,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: "misalnya: Downey Jr.'s ultah",
+                hintStyle: TextStyle(color: Colors.white54),
+                filled: true,
+                fillColor: Color(0xFF2F4B53),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              "Kapan?",
+              style: GoogleFonts.dmSans(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFFEAFFF9),
+              )),
+            const SizedBox(height: 12),
+            GestureDetector(
+              onTap: () => _selectDate(context),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Color(0xFF62838E),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min, // Biar gak melebar penuh
+                  children: [
+                    Icon(
+                      Icons.calendar_month,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      selectedDate != null
+                          ? "Tanggal: ${DateFormat.yMMMd().format(selectedDate!)}"
+                          : "Pilih tanggal",
+                      style: GoogleFonts.dmSans(color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFEAFFF9),
+                  padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: _addWhen,
+                child: Text(
+                  "Add",
+                  style: GoogleFonts.dmSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF213339),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class _BirthdaysPageState extends State<BirthdaysPage> {
@@ -26,7 +169,7 @@ class _BirthdaysPageState extends State<BirthdaysPage> {
     super.initState();
     _birthdayEvents = {};
 
-    for (var when in widget.whenList) {
+    for (var when in globalWhenList) {
       if (when['category'] == 'Birthdays' && when['date'] != null) {
         final DateTime date = DateTime(
           when['date'].year,
@@ -60,6 +203,23 @@ class _BirthdaysPageState extends State<BirthdaysPage> {
           ),
         ),
         foregroundColor: const Color(0xFFEAFFF9),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () async {
+              final result = Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddBirthdayPage()),
+              );
+
+              if (result != null) {
+                await saveWhenToHive();
+                loadWhenFromHive();
+                setState(() {}); // refresh kalender
+              }
+            },
+          ),
+        ],
       ),
       body: Column(
         children: [
@@ -157,6 +317,8 @@ class _BirthdaysPageState extends State<BirthdaysPage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+
+              // Home
               IconButton(
                 onPressed: () {
                   Navigator.pushReplacement(
@@ -167,28 +329,37 @@ class _BirthdaysPageState extends State<BirthdaysPage> {
                 icon: Icon(Icons.home),
                 color: Color(0xFFEAFFF9),
               ),
+
+              // Ultah
               IconButton(
                 onPressed: () {
-                  Navigator.pushReplacement(
+                  final birthdayList = globalWhenList
+                      .where((item) => item['category'] == 'Birthdays' && item['date'] != null)
+                      .toList();
+
+                  Navigator.pushReplacementNamed(
                     context,
-                    MaterialPageRoute(
-                      builder: (context) => BirthdaysPage(whenList: pinnedWhenList),
-                    ),
+                    '/birthdays',
+                    arguments: birthdayList,
                   );
                 },
                 icon: Icon(Icons.cake),
                 color: Color(0xFFEAFFF9),
               ),
+
+              // Your When
               IconButton(
                 onPressed: () {
                   Navigator.pushReplacement(
                     context,
-                    MaterialPageRoute(builder: (context) => YourWhenPage(whenList: pinnedWhenList)),
+                    MaterialPageRoute(builder: (context) => YourWhenPage()),
                   );
                 },
                 icon: Icon(Icons.app_registration_sharp),
                 color: Color(0xFFEAFFF9),
               ),
+
+              // Holidays
               IconButton(
                 onPressed: () {
                   Navigator.pushReplacementNamed(context, '/holidays');
@@ -196,11 +367,13 @@ class _BirthdaysPageState extends State<BirthdaysPage> {
                 icon: Icon(Icons.calendar_month),
                 color: Color(0xFFEAFFF9),
               ),
+
+              // Explore When Ideas
               IconButton(
                 onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/profile');
+                  Navigator.pushReplacementNamed(context, '/ideas');
                 },
-                icon: Icon(Icons.person),
+                icon: Icon(Icons.lightbulb),
                 color: Color(0xFFEAFFF9),
               ),
             ],
